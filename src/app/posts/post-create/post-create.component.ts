641f1ae3fe82ccb31dcf961f721d9243b02ae6d7
hglchen <hglchen@uwaterloo.ca>
2020-08-29 01:42:34 -0400

    Added create component.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,24 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { PostsService } from '../posts.service';
+import { MessageService } from 'primeng/api';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  styleUrls: ['./post-create.component.css'],
+  providers: [MessageService]
 })
 export class PostCreateComponent implements OnInit {
 
-  constructor(public postsService: PostsService) { }
+  private mode = 'create';
+  private postId: string;
+  post: Post;
+
+  constructor(public postsService: PostsService,
+    private messageService: MessageService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.postsService.getPost(this.postId).subscribe(res => {
+          this.post = {id: res._id, title: res.title, content: res.content};
+        });
+      } else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
   }
 
-  onAddPost(form: NgForm) {
+  onSavePost(form: NgForm) {
     if (form.invalid) {
+      this.messageService.add({ severity: 'error', summary: 'Error!', detail: 'Your entry is invalid.' });
       return;
     }
-    this.postsService.addPost(form.value.title, form.value.content);
+    if (this.mode === 'create') {
+      this.postsService.addPost(form.value.title, form.value.content);
+    } else if (this.mode === 'edit') {
+      this.postsService.updatePost(this.postId, form.value.title, form.value.content);
+    }
+    this.messageService.add({ severity: 'success', summary: 'Success!', detail: 'Your entry has been saved.' });
     form.resetForm();
   }
 
