3f0e24bc19c443d846ffed5af0cbdd7d6a62eb71
hglchen <hglchen@uwaterloo.ca>
2020-08-30 02:28:08 -0400

    Added github links.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AuthInterceptor } from './auth/auth-interceptor';
 import { PrimeNgModule } from './primeng.module';
 import { PostsModule } from './posts/posts.module';
 
+
 export function playerFactory() {
     return player;
 }
