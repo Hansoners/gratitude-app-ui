81a43388368ac1d4183715e180d5555895f29f1f
hglchen <hglchen@uwaterloo.ca>
2020-08-29 20:31:52 -0400

    Updated header to reflect auth.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private authListenerSubs: Subscription;
+  userIsAuth = false;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authListenerSubs = this.authService.getAuthStatusListener().subscribe(isAuth => {
+      this.userIsAuth = isAuth;
+    });
   }
+
+  ngOnDestroy(): void {
+    this.authListenerSubs.unsubscribe();
+  }
+
 
 }
